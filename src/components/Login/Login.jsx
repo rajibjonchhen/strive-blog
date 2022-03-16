38adc952564c6f5b2b5914fe61bda0a5b72fc1f4
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-03-16 16:21:08 +0000

    login added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -9,10 +9,17 @@ const Login = () => {
         email:''
     })
 
+    const [userLogin, setUserLogin] = useState({
+        email:'',
+        password:''
+    })
+
+const [login, setLogin] = useState(false)
+
 const createAuthor = async() => {
 const url = "http://localhost:3001"
 
-const response = await fetch(`${url}/authors`,{
+const response = await fetch(`${url}/authors/register`,{
     method:"POST",
     body:JSON.stringify(author),
     headers:{
@@ -31,25 +38,68 @@ if(response.ok){
         // window.location.href = `http://localhost:3000/home/${data._id}`;
 
         navigate(`/home/${data._id}`)
-
-
-
     }
 }
+}
+
+const loginFunc = async(e) => {
+        const url = "http://localhost:3001"
+        
+        const response = await fetch(`${url}/authors/login`,{
+            method:"POST",
+            body:JSON.stringify(userLogin),
+            headers:{
+                "Content-Type" : "application/json",
+            }
+        })
+        
+        if(response.ok){
+            const {token, _id} = await response.json()
+            if(token){
+                console.log(token)
+                localStorage.setItem("token",token);
+                setUserLogin({
+                    name:'',
+                    email:''
+                })
+            
+                // window.location.href = `http://localhost:3000/home/${data._id}`;
+        
+                navigate(`/home/${_id}`)
+            }
+        }
 }
 
     return(<Container>
             <Row>
                 <Col sm={12} md={8} lg={4} className="m-auto">
-                <div className="d-flex flex-column mt-5 " style={{height:'500px', paddingTop:'200px'}}>
-                    <p className='h3'>Enter detail to login</p>
-                    <label htmlFor="name"> Full Name</label>
-                    <input type="text" id="name" name="name" value={author.name} onChange={(e) => setAuthor({...author,name:e.target.value})}/>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" id="email" name="email" value={author.email} onChange={(e) => setAuthor({...author,email:e.target.value})}/>
-                    <div className='mt-4 m-auto'>
-                    <span className='btn btn-outline-primary' onClick={(e) => createAuthor(e)}>Confirm</span>
-                    
+                <div className="mt-5 " style={{height:'500px', paddingTop:'200px'}}>
+                    <button onClick={() => setLogin(false)}>Register</button>
+                    <button onClick={() => setLogin(true)}>Login</button>
+                    <div style={{display:!login? "block":"none"}}>
+                        <div className="d-flex flex-column mt-5 " style={{height:'500px', paddingTop:'20px'}} >
+                            <p className='h3'>Enter detail to Register</p>
+                            <label htmlFor="name"> Full Name</label>
+                            <input type="text" id="name" name="name" value={author.name} onChange={(e) => setAuthor({...author,name:e.target.value})}/>
+                            <label htmlFor="email">Email</label>
+                            <input type="text" id="email" name="email" value={author.email} onChange={(e) => setAuthor({...author,email:e.target.value})}/>
+                            <div className='mt-4 m-auto'>
+                            <span className='btn btn-outline-primary' onClick={(e) => createAuthor(e)}>Confirm</span>
+                            </div>
+                        </div>
+                    </div>
+                    {/* register */}
+                    <div style={{display:login? "block":"none"}}>
+                        <div className="d-flex flex-column mt-5 " style={{height:'500px', paddingTop:'20px'}} >
+                            <p className='h3'>Enter detail to Login</p>
+                            <label htmlFor="userName">Email</label>
+                            <input type="text" id="userName" name="email" value={userLogin.email} onChange={(e) => setUserLogin({...userLogin,email:e.target.value})}/>
+                            <label htmlFor="password"> Password</label>
+                            <input type="password" id="userName" name="password" value={userLogin.password} onChange={(e) => setUserLogin({...userLogin,password:e.target.value})}/>
+                            <div className='mt-4 m-auto'>
+                            <span className='btn btn-outline-primary' onClick={(e) => loginFunc(e)}>Confirm</span>
+                            </div>
+                        </div>
                     </div>
                     {/* <label for="avatar">Avatar</label>
                     <input type="file" id="avatar" name="avatar" value=""/> */}
