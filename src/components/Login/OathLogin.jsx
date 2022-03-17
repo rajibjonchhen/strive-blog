2f96cf33ee54cb5c3d85210b6ba6c60cd8b106f5
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-03-17 14:04:57 +0000

    href added from back to direct to google oath

--- src/components/Login/OathLogin.jsx
+++ src/components/Login/OathLogin.jsx
@@ -6,9 +6,15 @@ import insta from '../../assets/instagram.png'
 function OathLogin() {
     return ( 
         <div className='text-center mt-2'>
+            <a href="http://localhost:3001/authors/googleLogin">
             <img className='oath-img pointer' src={google} alt/>
+            </a>
+            <a href="/">
             <img className='oath-img pointer' src={insta} alt/>
+            </a>
+            <a href="/">
             <img className='oath-img pointer' src={facebook} alt/>
+            </a>
         </div>
      );
 }
